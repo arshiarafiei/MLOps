0a565f6c52bfc1c1dbb031721b072b14582ed666
tart msu <tartmsu@ali-mac.dhcp.egr.msu.edu>
2025-09-09 22:50:55 -0400

    First Model with 2X

--- HMW1/src/main.py
+++ HMW1/src/main.py
@@ -25,6 +25,31 @@ def main():
     print("Predictions 1 X:", predictions_1)
 
 
+    data = pd.read_csv('data/sampregdata.csv') 
+
+
+    X = data[['x1','x2']]
+    y = data['y']
+
+
+    model_X2 = LR()
+
+
+    model_X2.fit(X, y)
+
+
+    predictions_2 = model_X2.predict(X)
+
+
+    print("Predictions 2 X:", predictions_2)
+
+
+    mse_1 = mean_squared_error(y, predictions_1)
+    mse_2 = mean_squared_error(y, predictions_2)
+    print("Mean Squared Error for 1 X:", mse_1)
+    print("Mean Squared Error for 2 X:", mse_2)
+
+
 
 if __name__ == "__main__":
     main()
